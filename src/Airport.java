5a0f12fc53ef5dfeb9e57830a4c6c348f052aee3
maxfrancis1 <max.francis2000@gmail.com>
2018-11-28 14:02:20 +0000

    Making a few tests and functions

--- src/Airport.java
+++ src/Airport.java
@@ -1,4 +1,19 @@
 // JAVA CODE
 
 public class Airport {
+
+    private int[] planes;
+
+    public Airport() {
+        this.planes = new int[] {0};
+    }
+
+    public String test() {
+        return "confirmed";
+    }
+
+    public int printPlanesArray() {
+        return this.planes[0];
+    }
+    
 }
--- tests/AirportTest.java
+++ tests/AirportTest.java
@@ -1,7 +1,21 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 //TESTING DOC
 
 public class AirportTest {
+
+    @Test
+    public void initiate() {
+        Airport x = new Airport();
+        assertEquals("confirmed", x.test());
+    }
+
+    @Test
+    public void checkArrayStartsEmpty() {
+        Airport x = new Airport();
+        assertEquals(0, x.printPlanesArray());
+    }
 
 }
